f9378af209556e8524e1b396e518a1111dca3e56
jekahamster <jekahamster@gmail.com>
2022-07-15 19:43:38 +0300

    Update defines

--- defines.py
+++ defines.py
@@ -1,3 +1,13 @@
 import os
+import pathlib
+from config.settings import Settings
+from config.hotkeys import HotKeys
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
+
+SETTINGS_PATH = ROOT_DIR / "config" / "settings.json"
+HOTKEYS_PATH = ROOT_DIR / "config" / "hotkeys.json"
+SETTINGS = Settings(SETTINGS_PATH)
+HOTKEYS = HotKeys(HOTKEYS_PATH)
+
+DB_PATH = pathlib.Path(SETTINGS.database)
