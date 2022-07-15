a62ca83472489f5938663fecb96609c889ee5ae9
jekahamster <jekahamster@gmail.com>
2022-07-15 19:46:08 +0300

    Add autocreation db if does not exist

--- storage_thread.py
+++ storage_thread.py
@@ -6,6 +6,15 @@ class StorageThread(QtCore.QThread):
         super().__init__(parent)
         self._storage = storage
         self._cursor = storage.cursor()
+        
+        creation_table_command = f"""
+            CREATE TABLE IF NOT EXISTS words (
+                `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                `word` TEXT NOT NULL,
+                `translation` TEXT NOT NULL
+            )
+        """
+        self._cursor.execute(creation_table_command)
 
     def append(self, src, dst):
         self._cursor.execute(f"INSERT INTO words (word, translation) VALUES (?, ?);", (src, dst))
