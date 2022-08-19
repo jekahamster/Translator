834d811ddca3d1231adafa56fd20dcd270416b76
jekahamster <jekahamster@gmail.com>
2022-08-20 02:06:29 +0300

    Reduce translation_thread.py functional and move to translator.py. translation_thread.py deleted now

--- translator.py
+++ translator.py
@@ -1,9 +1,27 @@
 import googletrans
 
+from logger_builder import get_logger
+from PyQt5 import QtCore
 
-class Translator:
-	def __init__(self, translator=googletrans.Translator()):
-		self._translator = translator
+logger = get_logger(__name__)
 
-	def translate(self, text, lang_src="en", lang_dst="ru"):
-		return self._translator.translate(text, src=lang_src, dest=lang_dst).text
+
+class Translator(QtCore.QObject):
+    translated_signal = QtCore.pyqtSignal(str, str)
+
+    def __init__(self, translator=googletrans.Translator()):
+        super().__init__()
+        self._translator = translator
+
+    def _translate(self, text, lang_src="en", lang_dst="ru"):
+        return self._translator.translate(text, src=lang_src, dest=lang_dst).text
+
+    def translate(self, text, lang_from, lang_to):
+        try:
+            stripped_text = text.strip()
+            translation_result = self._translate(text, lang_from, lang_to).strip()
+            self.translated_signal.emit(stripped_text, translation_result)
+        except Exception as e:
+            self._dst_text = ""
+            logger.info(f"Cant translate '{text}'")
+            logger.info(e)
